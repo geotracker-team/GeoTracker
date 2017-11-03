e60e8c9e6d3a0d2b6b08c246df3b47752fbd4419
joanj94 <joanj_94@hotmail.com>
2017-11-03 20:26:42 +0100

    - Added intent to go from login to menu

--- app/src/main/java/com/juanjo/udl/geotracker/LoginActivity.java
+++ app/src/main/java/com/juanjo/udl/geotracker/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.juanjo.udl.geotracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(LoginActivity.this, MenuActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
