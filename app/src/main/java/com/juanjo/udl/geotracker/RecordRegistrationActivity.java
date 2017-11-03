9b9d9fbd3fec506d997c61e4a3d0a9447191c3b8
joanj94 <joanj_94@hotmail.com>
2017-11-03 20:37:34 +0100

    - Added record registration Activity class

---
+++ app/src/main/java/com/juanjo/udl/geotracker/RecordRegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.juanjo.udl.geotracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecordRegistrationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_registration);
+
+    }//onCreate
+}
