c070f11a9cc9f1c6b33d47ceede134ed0d2e1980
juanjo <canela@opennatur.com>
2017-11-06 12:12:40 +0100

    - Added AddField activity

---
+++ app/src/main/java/com/juanjo/udl/geotracker/AddFieldActivity.java
@@ -0,0 +1,13 @@
+package com.juanjo.udl.geotracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddFieldActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_field);
+    }//onCreate
+}
--- app/src/main/java/com/juanjo/udl/geotracker/RecordRegistrationActivity.java
+++ app/src/main/java/com/juanjo/udl/geotracker/RecordRegistrationActivity.java
@@ -1,7 +1,10 @@
 package com.juanjo.udl.geotracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecordRegistrationActivity extends Activity {
 
@@ -10,5 +13,13 @@ public class RecordRegistrationActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_registration);
 
+        Button btnAddField = findViewById(R.id.btnaddfield);
+        btnAddField.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecordRegistrationActivity.this, AddFieldActivity.class);
+                startActivity(intent);
+            }
+        });
     }//onCreate
 }
