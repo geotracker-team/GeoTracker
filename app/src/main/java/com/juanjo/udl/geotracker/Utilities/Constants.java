5a5ebf3c766d0dd3cb1619151940383811bfdd02
juanjo <canela@opennatur.com>
2017-11-09 09:24:32 +0100

    - Add static class for constants

---
+++ app/src/main/java/com/juanjo/udl/geotracker/Utilities/Constants.java
@@ -0,0 +1,9 @@
+package com.juanjo.udl.geotracker.Utilities;
+
+public class Constants {
+    public static class StaticFields{
+        private static final String folderOfRecords = "/records";
+
+        public static String getFolderOfRecords() { return folderOfRecords; }//getFolderOfRecords
+    }//StaticFields
+}//Constants
