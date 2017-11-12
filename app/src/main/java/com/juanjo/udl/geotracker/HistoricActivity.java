ae859b3183f15ee9980e22c0a2eedb453191ff0a
dsm9 <dsm9@alumnes.udl.cat>
2017-11-12 09:32:02 +0100

    Connection with JSON classes

--- app/src/main/java/com/juanjo/udl/geotracker/HistoricActivity.java
+++ app/src/main/java/com/juanjo/udl/geotracker/HistoricActivity.java
@@ -2,18 +2,48 @@ package com.juanjo.udl.geotracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import com.juanjo.udl.geotracker.JSONObjects.JSONRecord;
+import com.juanjo.udl.geotracker.JSONObjects.JSONRecordAdapter;
+import com.juanjo.udl.geotracker.Utilities.Constants;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class HistoricActivity extends AppCompatActivity {
+
+    EditText fUser;
+    EditText fProject;
+    EditText fDateIni;
+    EditText fDateFin;
+    Button btSearch;
+
+    ArrayList<JSONRecord> records;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historic);
 
+//        deleteRecords();
+//        createExampleRegs();
+
+        fUser = findViewById(R.id.fUser);
+        fProject = findViewById(R.id.fProject);
+        fDateIni = findViewById(R.id.fDateIni);
+        fDateFin = findViewById(R.id.fDateFin);
+
+        records = readRecords();
+
+        JSONRecordAdapter itemsAdapter = new JSONRecordAdapter(this, records);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+
+/*
         ArrayList<Register> registers = new ArrayList<Register>();
 
         registers.add(new Register("01/10/2017","David","EPS project ",  new Double(41.6109), new Double(0.6419), "Track point 1"));
@@ -25,8 +55,77 @@ public class HistoricActivity extends AppCompatActivity {
         RegisterAdapter itemsAdapter = new RegisterAdapter(this, registers);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
-
+*/
 
 
     }
+
+
+    private boolean deleteRecords()  {
+
+        ArrayList<JSONRecord> records = new ArrayList<JSONRecord>();
+        try {
+
+            File dir = new File(getFilesDir().getCanonicalPath() + Constants.StaticFields.getFolderOfRecords());
+            File[] files = dir.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    file.delete();
+                }
+            }
+
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private ArrayList<JSONRecord> readRecords()  {
+
+        ArrayList<JSONRecord> records = new ArrayList<JSONRecord>();
+        try {
+
+        File dir = new File(getFilesDir().getCanonicalPath() + Constants.StaticFields.getFolderOfRecords());
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    records.add(new JSONRecord(this, file));
+                }
+            }
+        }
+
+        return records;
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private void createExampleRegs() {
+
+        JSONRecord record = null;
+        try {
+//            File dir = new File(getFilesDir().getCanonicalPath() + Constants.StaticFields.getFolderOfRecords());
+//            File[] files = dir.listFiles();
+//            if (files.length == 0) {
+                record = new JSONRecord(this, "Track point 1", "01-10-2017 10:15:00", "David", new Double(41.6109), new Double(0.6419));
+                record.save();
+                record = new JSONRecord(this, "Track point 2", "01-10-2017 11:23:00", "David", new Double(41.6121), new Double(0.6408));
+                record.save();
+                record = new JSONRecord(this, "Track point 3", "02-10-2017 11:23:00", "David", new Double(41.6111), new Double(0.6404));
+                record.save();
+                record = new JSONRecord(this, "Track point 4", "03-10-2017 11:23:00", "Joan Josep", new Double(41.6109), new Double(0.646));
+                record.save();
+                record = new JSONRecord(this, "Track point 5", "04-10-2017 11:23:00", "Xavier", new Double(41.6107), new Double(0.6412));
+                record.save();
+                record = new JSONRecord(this, "Track point 5", "05-10-2017 11:23:00", "Dejan", new Double(41.6103), new Double(0.6414));
+                record.save();
+  //          }
+        } catch (Exception e) {
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/juanjo/udl/geotracker/JSONObjects/JSONRecordAdapter.java
@@ -0,0 +1,67 @@
+package com.juanjo.udl.geotracker.JSONObjects;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.juanjo.udl.geotracker.R;
+import com.juanjo.udl.geotracker.RecordViewActivity;
+import com.juanjo.udl.geotracker.Register;
+
+import java.util.ArrayList;
+
+/**
+ * Created by David on 05/11/2017.
+ */
+
+public class JSONRecordAdapter extends ArrayAdapter {
+
+    public JSONRecordAdapter(Context context, ArrayList<JSONRecord> registers) {
+        super(context, 0, registers);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View listItemView = convertView;
+
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item , parent, false);
+        }
+
+        JSONRecord currentRec = (JSONRecord) getItem(position);
+
+        listItemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getContext(), RecordViewActivity.class);
+                getContext().startActivity(it);
+            }
+        });
+/*
+        TextView projectTextView = (TextView) listItemView.findViewById(R.id.project);
+        projectTextView.setText(currentRec.getProjectName());
+*/
+        TextView userTextView = (TextView) listItemView.findViewById(R.id.user);
+        userTextView.setText(currentRec.getUsername());
+
+        TextView dateTextView = (TextView) listItemView.findViewById(R.id.date);
+        dateTextView.setText(currentRec.getDate());
+
+        TextView latitudeTextView = (TextView) listItemView.findViewById(R.id.latitude);
+        latitudeTextView.setText(currentRec.getLatitude().toString());
+
+        TextView longitudeTextView = (TextView) listItemView.findViewById(R.id.longitude);
+        longitudeTextView.setText(currentRec.getLongitude().toString());
+
+        TextView descripTextView = (TextView) listItemView.findViewById(R.id.description);
+        descripTextView.setText(currentRec.getDescription());
+
+        return listItemView;
+    }
+
+}
