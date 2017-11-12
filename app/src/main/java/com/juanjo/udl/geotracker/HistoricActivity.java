da0d60405d2d306c444a56d40f243d32b7bafa25
dsm9 <dsm9@alumnes.udl.cat>
2017-11-12 10:02:25 +0100

    Class to create and delete sample records

--- app/src/main/java/com/juanjo/udl/geotracker/HistoricActivity.java
+++ app/src/main/java/com/juanjo/udl/geotracker/HistoricActivity.java
@@ -9,6 +9,7 @@ import android.widget.ListView;
 import com.juanjo.udl.geotracker.JSONObjects.JSONRecord;
 import com.juanjo.udl.geotracker.JSONObjects.JSONRecordAdapter;
 import com.juanjo.udl.geotracker.Utilities.Constants;
+import com.juanjo.udl.geotracker.Utilities.SampleData;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -29,8 +30,9 @@ public class HistoricActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historic);
 
-//        deleteRecords();
-//        createExampleRegs();
+//        SampleData sample = new SampleData();
+//        sample.delete(this);
+//        sample.create(this);
 
         fUser = findViewById(R.id.fUser);
         fProject = findViewById(R.id.fProject);
@@ -61,26 +63,6 @@ public class HistoricActivity extends AppCompatActivity {
     }
 
 
-    private boolean deleteRecords()  {
-
-        ArrayList<JSONRecord> records = new ArrayList<JSONRecord>();
-        try {
-
-            File dir = new File(getFilesDir().getCanonicalPath() + Constants.StaticFields.getFolderOfRecords());
-            File[] files = dir.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    file.delete();
-                }
-            }
-
-            return true;
-
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
     private ArrayList<JSONRecord> readRecords()  {
 
         ArrayList<JSONRecord> records = new ArrayList<JSONRecord>();
@@ -100,31 +82,6 @@ public class HistoricActivity extends AppCompatActivity {
 
         } catch (Exception e) {
             return null;
-        }
-    }
-
-    private void createExampleRegs() {
-
-        JSONRecord record = null;
-        try {
-//            File dir = new File(getFilesDir().getCanonicalPath() + Constants.StaticFields.getFolderOfRecords());
-//            File[] files = dir.listFiles();
-//            if (files.length == 0) {
-                record = new JSONRecord(this, "Track point 1", "01-10-2017 10:15:00", "David", new Double(41.6109), new Double(0.6419));
-                record.save();
-                record = new JSONRecord(this, "Track point 2", "01-10-2017 11:23:00", "David", new Double(41.6121), new Double(0.6408));
-                record.save();
-                record = new JSONRecord(this, "Track point 3", "02-10-2017 11:23:00", "David", new Double(41.6111), new Double(0.6404));
-                record.save();
-                record = new JSONRecord(this, "Track point 4", "03-10-2017 11:23:00", "Joan Josep", new Double(41.6109), new Double(0.646));
-                record.save();
-                record = new JSONRecord(this, "Track point 5", "04-10-2017 11:23:00", "Xavier", new Double(41.6107), new Double(0.6412));
-                record.save();
-                record = new JSONRecord(this, "Track point 5", "05-10-2017 11:23:00", "Dejan", new Double(41.6103), new Double(0.6414));
-                record.save();
-  //          }
-        } catch (Exception e) {
-
         }
     }
 
