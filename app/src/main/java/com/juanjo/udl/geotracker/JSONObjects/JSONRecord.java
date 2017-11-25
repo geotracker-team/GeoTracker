24ca475d7e29c6b3925f4f2ab6f0d2f1d540669e
joanj94 <joanj_94@hotmail.com>
2017-11-25 15:11:37 +0100

    - Fixed error loading JSONRecord that affects userName and projectName

--- app/src/main/java/com/juanjo/udl/geotracker/JSONObjects/JSONRecord.java
+++ app/src/main/java/com/juanjo/udl/geotracker/JSONObjects/JSONRecord.java
@@ -66,10 +66,10 @@ public class JSONRecord extends JSONGlobal {
         this.context = context;
         this.description = jsonObject.getString("description");
         this.date = jsonObject.getString("date");
-        this.userId = jsonObject.getInt("userid");
-        this.userName = jsonObject.getString("username");
-        this.projectId = jsonObject.getInt("projectid");
-        this.projectName = jsonObject.getString("projectname");
+        this.userId = jsonObject.has("userId") ? jsonObject.getInt("userId") : 0;
+        this.userName = jsonObject.has("userName") ? jsonObject.getString("userName") : "";
+        this.projectId = jsonObject.has("projectId") ? jsonObject.getInt("projectId") : 0;
+        this.projectName = jsonObject.has("projectName") ? jsonObject.getString("projectName") : "";
         this.latitude = jsonObject.getDouble("latitude");
         this.longitude = jsonObject.getDouble("longitude");
         this.otherFields = new JSONObjectImplSerializable(jsonObject.getJSONObject("otherFields"));
@@ -80,10 +80,10 @@ public class JSONRecord extends JSONGlobal {
     private void putValues() throws JSONException {
         put("description", description);
         put("date", date);
-        put("userid", userId);
+        put("userId", userId);
         put("username", userName);
-        put("projectid", projectId);
-        put("projectname", projectName);
+        put("projectId", projectId);
+        put("projectName", projectName);
         put("latitude", latitude);
         put("longitude", longitude);
 
