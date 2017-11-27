53f976858fc48566ba3b6a48517bd27792a1d214
Joan Josep Jiménez <joanj_94@hotmail.com>
2017-11-27 21:49:23 +0100

    Update JSONRecord.java

--- app/src/main/java/com/juanjo/udl/geotracker/JSONObjects/JSONRecord.java
+++ app/src/main/java/com/juanjo/udl/geotracker/JSONObjects/JSONRecord.java
@@ -68,10 +68,10 @@ public class JSONRecord extends JSONGlobal {
         this.context = context;
         this.description = jsonObject.getString("description");
         this.date = jsonObject.getString("date");
-        this.userId = jsonObject.has("userid") ? jsonObject.getInt("userid") : 0;
-        this.userName = jsonObject.has("username") ? jsonObject.getString("username") : "";
-        this.projectId = jsonObject.has("projectid") ? jsonObject.getInt("projectid") : 0;
-        this.projectName = jsonObject.has("projectname") ? jsonObject.getString("projectname") : "";
+        this.userId = jsonObject.has("userId") ? jsonObject.getInt("userId") : 0;
+        this.userName = jsonObject.has("userName") ? jsonObject.getString("userName") : "";
+        this.projectId = jsonObject.has("projectId") ? jsonObject.getInt("projectId") : 0;
+        this.projectName = jsonObject.has("projectName") ? jsonObject.getString("projectName") : "";
         this.latitude = jsonObject.getDouble("latitude");
         this.longitude = jsonObject.getDouble("longitude");
         this.otherFields = (HashMap<String, Object>) Constants.AuxiliarFunctions.jsonToMap(jsonObject.getJSONObject("otherFields"));
